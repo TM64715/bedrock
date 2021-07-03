8d100622ad5d944baf10bf15d2325e52fc651721
TM64715 <tage.mehta@gmail.com>
2021-07-03 10:43:38 -0400

    video working

---
+++ pages/api/rooms/archive.js
@@ -0,0 +1,10 @@
+import nc from 'next-connect';
+import roomsDAO from '../../../dao/roomsDAO';
+import { all } from '../../../middleware';
+
+const handler = nc();
+handler.use(all);
+handler.put(async (req, res) => {
+  const { id } = req.body;
+  res.json(await roomsDAO.archive(id));
+});
